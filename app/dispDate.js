2f5dfb18465b19e2fca5bb15bde56e1b229ea560
Fredrikwide <Fredrik.widegren@gmail.com>
2020-01-14 13:34:18 +0100

    updated fetch function async/await and some node modules for moment.js and firebase i think

--- app/dispDate.js
+++ app/dispDate.js
@@ -2,9 +2,6 @@
 
 FORM.addEventListener('submit', function(e){
     e.preventDefault();
-    getDate().then( info => {
-        info.forEach(response.name, function(){
-            console.log('this is my name ');
-        });
-    });
+    getData();
 });
+
--- app/getDate.js
+++ app/getDate.js
@@ -1,10 +1,20 @@
 const searchBar = document.querySelector('#search');
 const dateForm = document.querySelector('#myDate');
 const FORM = document.querySelector('#searchForm');
+const CHOOSE_COUNTRY = document.querySelector('#countryList');
 
-const getDate = async function (){
-    const response = await fetch(`https://api.abalin.net/getdate?name=${searchBar.value}&country=us`);
-    const data = await response.json();
-    return data;
+
+const getData = async (date) => {
+    
+    const response = await fetch(`https://api.abalin.net/getdate?name=${searchBar.value}&country=${CHOOSE_COUNTRY.value}`);
+       
+        console.log(response);
+        if (!response.ok) {
+            throw new Error(`Response was not OK. Status Code was ${response.status}.`);
+        }
+
+    const data = response.json();
+
+    console.log(data);
+
 }
-
