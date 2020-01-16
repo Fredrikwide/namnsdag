7e661f898432729d1e51a031b137ab75b4fe159c
Fredrikwide <Fredrik.widegren@gmail.com>
2020-01-16 12:10:04 +0100

    progress is made thru suffering

--- app/dispDate.js
+++ app/dispDate.js
@@ -1,7 +1,92 @@
+const resultDiv = document.querySelector('#showMe');
+var searchBar = document.querySelector('#search');
+const FORM = document.querySelector('#searchForm');
+
+const CHOOSE_COUNTRY = document.querySelector('#countryList');
+const CHOOSE_MONTH = document.querySelector('#monthDrop');
+const CHOOSE_DAY = document.querySelector('#dayDrop');
+
+var clg = console.log;
+
+const corsAnywhere = 'https://cors-anywhere.herokuapp.com/'
+let fetchURL;
+
+var chosenCountry;
+var chosenDay;
+var chosenMonth;
 
 
-FORM.addEventListener('submit', function(e){
+CHOOSE_COUNTRY.addEventListener('change', function (e) {
+    chosenCountry = CHOOSE_COUNTRY.options[CHOOSE_COUNTRY.selectedIndex].value;
+    clg(chosenCountry);
+
+
+})
+
+CHOOSE_DAY.addEventListener('change', function (e) {
+    chosenDay = CHOOSE_DAY.options[CHOOSE_DAY.selectedIndex].text;
+    clg(chosenDay);
+
+})
+
+CHOOSE_MONTH.addEventListener('change', function (e) {
+    chosenMonth = CHOOSE_MONTH.value;
+    clg(chosenMonth);
+
+})
+
+
+
+
+const getURL = function (country, day, month) {
+
+    if (country !== 0 && searchBar.value) {
+        return fetchURL = corsAnywhere + `https://api.abalin.net/getdate?name=${searchBar.value}&country=${chosenCountry}`;
+    }
+
+    else if ((country !== 0 && day !== 0 && month !== 0)) {
+        return fetchURL = corsAnywhere + `https://api.abalin.net/namedays?country=${chosenCountry}&month=${chosenMonth}&day=${chosenDay}`;
+    }
+
+    else {
+        throw new Error('search for either name and country or day/month and country');
+    }
+
+}
+
+
+
+FORM.addEventListener('submit', function (e) {
     e.preventDefault();
-    getData();
+    let calcUrl = getURL(chosenCountry, chosenDay, chosenMonth);
+    clg(`chosen country is : ${chosenCountry}, chosen month is : ${chosenMonth}, chosen day is : ${chosenDay}`);
+    resultDiv.innerHTML = "";
+    getData(calcUrl).then(data => {
+        console.log(data);
+        if(calcUrl === corsAnywhere + `https://api.abalin.net/getdate?name=${searchBar.value}&country=${chosenCountry}`)
+        {data.results.forEach(elem => {
+            if(elem.name.includes(searchBar.value)){
+                let kirby = elem.name.indexOf(searchBar.value);
+                let bandanaWaddleDee = kirby + searchBar.value.length;
+                let chefKawasaki = elem.name.slice(kirby, bandanaWaddleDee);
+                const renderHTML = `<p>${chefKawasaki}s name day is <span>${elem.day}&#47;${elem.month}</span></p>`;
+                resultDiv.innerHTML += renderHTML;
+            }
+            else {
+                clg('no name matches the search');
+            }
+        });
+    }
+    else {
+        data.data.forEach(day => {
+            const CC = chosenCountry;
+            const createHTML = `<p> in ${CHOOSE_COUNTRY.options[CHOOSE_COUNTRY.selectedIndex].text} on 
+            ${day.dates.day}&#47;${day.dates.month} the name ${day.namedays[CC]} has a nameday</p>`;
+            resultDiv.innerHTML += createHTML;
+        })
+    }
+    }).catch(err => {
+        const errHTML = `<h1>somehting went wrong :( ${err}</h1>`
+        resultDiv.innerHTML += errHTML;
+    })
 });
-
--- app/getDate.js
+++ app/getDate.js
@@ -1,20 +1,5 @@
-const searchBar = document.querySelector('#search');
-const dateForm = document.querySelector('#myDate');
-const FORM = document.querySelector('#searchForm');
-const CHOOSE_COUNTRY = document.querySelector('#countryList');
-
-
-const getData = async (date) => {
-    
-    const response = await fetch(`https://api.abalin.net/getdate?name=${searchBar.value}&country=${CHOOSE_COUNTRY.value}`);
-       
-        console.log(response);
-        if (!response.ok) {
-            throw new Error(`Response was not OK. Status Code was ${response.status}.`);
-        }
-
-    const data = response.json();
-
-    console.log(data);
-
+const getData = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
 }
